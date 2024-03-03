9b41d87d46ba3b54f4dcaa397f21cfb0a1386660
Safdar420 <160203243+Safdar420@users.noreply.github.com>
2024-03-03 03:30:45 -0800

    Add files via upload

---
+++ app3.js
@@ -0,0 +1,84 @@
+// const Name2 : string = "Safdar Ali";
+// console.log(`Name : ${Name2}`);
+// const F_Name3 : string = "Sardar Ali";
+// console.log(`Father_Name : ${F_Name3}`);
+// let Class : number = 12;
+// console.log(`Class : ${Class}`);
+// Class = 13;
+// console.log(`Class : ${Class}`);
+// let College : string = "Islamia Govt Science College";
+// console.log(`College : ${College}`);
+// College = "D-J Science College";
+// console.log(`College : ${College}`);
+// let email_address : string = "sa5763085@gmail.com";
+// console.log(`Email Adress : ${email_address}`);
+// email_address = "aa6865674@gmail.com";
+// console.log(`Email Adress : ${email_address}`);
+// let Phone_no : number = +923442844068;
+// console.log(`Phone NO : ${Phone_no}`);
+// Phone_no = +923408621402   
+// console.log(`Phone NO : ${Phone_no}`);
+// when i update my name in variable using let my name has update
+// let  Name11 : string = "Safdar";
+// console.log(`Name : ${Name11}`);
+// Name11 = "Sardar Safdar";
+// console.log(`Name : ${Name11}`);
+// Name11 = "Sardar Safdar Ali";
+// console.log(`Name : ${Name11}`);
+// Name11 = "Sardar Safdar Ali Khan";
+// console.log(`Name : ${Name11}`);
+// father name updated list
+// let Father_Name :string = "Sardar"
+// console.log(`Father Name : ${Father_Name}`);
+// Father_Name = "Sardar Ali";
+// console.log(`Father Name : ${Father_Name}`);
+// Father_Name = "Sardar Mawaz";
+//  console.log(`Father Name : ${Father_Name}`);
+//  Father_Name = "Sardar Mawaz Khan";
+// console.log(`Father Name : ${Father_Name}`);
+// update class 
+// let Class1 : number = 9;
+// console.log(`Class : ${Class1}`);
+// Class1  = 10;
+// console.log(`Class : ${Class1}`);
+// Class1  = 11;
+// console.log(`Class : ${Class1}`);
+// Class1  = 12;
+// console.log(`Class : ${Class1}`);
+// updated college :
+// let college1 : string = "Islamia science college";
+// console.log(`College : ${college1}`);
+// college1 = "Islamia science college Karachi";
+// console.log(`College : ${college1}`);
+// college1 = "Gov Islamia science college Karachi";
+// console.log(`College : ${college1}`);
+// college1 = "Government Islamia science college Karachi";
+// console.log(`College : ${college1}`);
+//  Email update
+// let email_address1 : string = "sa5763085@gmail.com";
+// console.log(`Email Adress : ${email_address1}`);
+// email_address1 = "aa6865674@gmail.com";
+// console.log(`Email Adress : ${email_address1}`);
+// email_address1 = "aa6865@gmail.com";
+// console.log(`Email Adress : ${email_address1}`);
+// email_address1 = "aa65674@gmail.com";
+// console.log(`Email Adress : ${email_address1}`);
+// update phone no 
+// let Phone_no1 : number = +923442844068;
+// console.log(`Phone NO : ${Phone_no1}`);
+// Phone_no1 = +923408621402   
+// console.log(`Phone NO : ${Phone_no1}`);
+// Phone_no1 = +923408621403   
+// console.log(`Phone NO : ${Phone_no1}`);
+// Phone_no1 = +923408621405   
+// console.log(`Phone NO : ${Phone_no1}`);
+var Name112 = "Safadr Ali";
+Name112 = "Safdar ali";
+Name112 = "safdar Ali";
+Name112 = "SaFdar Ali";
+console.log("Name : ".concat(Name112));
+var Name111 = "Sardar Safdar Ali Khan";
+var F_Name111 = "Sardar Ali";
+var Age = 20;
+var Mann = ("Name : ".concat(Name111, " , FatherName : ").concat(F_Name111, " , age : ").concat(Age));
+console.log(Mann);
